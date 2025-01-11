341aaa28d81f7753c722d455a0b597049fbbac41
2377310026 <bu2377310026@bukkyo-u.ac.jp>
2025-01-11 08:58:48 +0000

    プログラミング1 1日目(2)

--- calk.rb
+++ calk.rb
@@ -1,10 +1,12 @@
-
+if ARGV.size == 0
+    puts "計算できません"
+    exit
+end
 i = 0
-
 while ARGV[i] != nil
     all = all.to_f + ARGV[i].to_f
     i = i+1
 end
 
 average = all.to_f/i
-puts average
+puts average.round(5)
